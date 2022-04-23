bc3227ff56b4806cf0206e75f91f620a84043168
Shubham Chaudhari <shubhamchaudhari88@gmail.com>
2022-04-24 02:31:21 +0530

    Time: 288 ms (55.85%), Space: 118.1 MB (52.31%) - LeetHub

--- 234-palindrome-linked-list/234-palindrome-linked-list.cpp
+++ 234-palindrome-linked-list/234-palindrome-linked-list.cpp
@@ -8,36 +8,62 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+
+
 class Solution {
     
-private:
-    bool checkPalidrome(vector<int> a)
-    {
-        int n=a.size();
-        int s=0;
-        int e=n-1;
+    
+public:
+    ListNode* getMiddle(ListNode* head){
+        ListNode* Fast = head->next;
+        ListNode* Slow = head;
         
-        while(s<=e)
-        {
-            if(a[s]!=a[e])
-            {
-               return false;
-            }
-            s++;
-            e--;
+        while(Fast != NULL && Fast->next){
+            Slow=Slow->next;
+            Fast=Fast->next->next;
         }
-        return true;
+        return Slow;
+    }
+    
+public:
+    ListNode* reverse(ListNode* head){
+        ListNode* current = head;
+        ListNode* prev = NULL;
+        ListNode* next = NULL;
+        
+        while(current!=NULL){
+           next = current->next;
+           current->next = prev;
+           prev = current;
+           current = next;
+        }
+        return prev;
     }
 public:
     bool isPalindrome(ListNode* head) {
-        ListNode *temp = head;
-        vector<int> arr;
-        while(temp!=NULL)
-        {
-            arr.push_back(temp->val);
-            temp = temp->next;
+       //check list is empty or having only 1 element
+        if(head == NULL || head->next == NULL)
+            return true;
+        
+        //step 1 : get middle element
+        ListNode* middle = getMiddle(head);
+        
+        //step 2: reverse list after mid
+        ListNode* temp = middle->next;
+        middle->next = reverse(temp);
+        
+        //step3 : compare both halves
+        ListNode* head1 = head;
+        ListNode* head2 = middle->next;
+        
+        while(head2!=NULL){
+            if(head1->val !=head2->val) return false;
+            
+            head1=head1->next;
+            head2=head2->next;
         }
-        return checkPalidrome(arr);
+        return true;
+        
         
     }
 };
