d1be098185404aad868c0ed8978c27ddfc6673f9
Shubham Chaudhari <shubhamchaudhari88@gmail.com>
2022-05-19 02:41:30 +0530

    Time: 3 ms (57.70%), Space: 5.8 MB (80.26%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
+    int memo[31]= {0};
     int fib(int n) {
-        if(n==0)
-            return 0;
-        else if (n==1)
-            return 1;
-        
-        else return fib(n-1) + fib(n-2);
+        if(n<=1)
+            return n;
+        if(memo[n])
+            return memo[n];
+        return memo[n] = fib(n-1) + fib(n-2);
         
     }
 };
