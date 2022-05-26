92eb40b88676eefc5bd5260083e12371cf7d1bc4
Shubham Chaudhari <shubhamchaudhari88@gmail.com>
2022-05-27 00:49:19 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (96.65%) - LeetHub

--- 191-number-of-1-bits/191-number-of-1-bits.cpp
+++ 191-number-of-1-bits/191-number-of-1-bits.cpp
@@ -4,8 +4,8 @@ public:
         int ans = 0;
         while(n>0)
         {
-            if(n%2==1) ans++;
-            n /= 2;
+            n &= n-1;
+            ans++;
         }
         return ans;
         
