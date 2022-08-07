f3062cfa2e357046834605ef1c150e33156559ec
Shubham Chaudhari <shubhamchaudhari88@gmail.com>
2022-08-07 23:59:10 +0530

    Time: 16 ms (38.45%), Space: 9.6 MB (56.99%) - LeetHub

--- 137-single-number-ii/137-single-number-ii.cpp
+++ 137-single-number-ii/137-single-number-ii.cpp
@@ -2,7 +2,10 @@ class Solution {
 public:
     int singleNumber(vector<int>& nums) {
         
+        int n = nums.size();
+        
         //Bruteforce approch 
+        /*
         for(int i=0; i< nums.size(); i++){
             bool found = false;
             for(int j=0; j<nums.size(); j++){
@@ -12,6 +15,21 @@ public:
             if(!found)
                 return nums[i];
         }
+        */
+        
+        //Sorting
+        sort(nums.begin(), nums.end());
+        if(n==1) return nums[0];
+        
+        if(nums[0]!=nums[1])
+            return nums[0];
+        if(nums[n-1]!=nums[n-2])
+            return nums[n-1];
+        
+        for(int i=1; i<n-1; i++){
+            if(nums[i]!=nums[i+1] && nums[i]!=nums[i-1])
+                return nums[i];
+        }                
      return -1;   
     }
 };
