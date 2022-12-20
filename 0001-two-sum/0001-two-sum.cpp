a030402599eb3a85f1d54be61c9ee076216c4427
Shubham Chaudhari <shubhamchaudhari88@gmail.com>
2022-12-20 23:18:38 +0530

    Time: 12 ms (92.69%), Space: 10.9 MB (30.67%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -4,24 +4,21 @@ public:
         vector<int> ans;
         
         int size = nums.size();
-        if(size==2)
-        {
-            ans.push_back(0);
-            ans.push_back(1);
-            return ans;
-        }   
-        
+        unordered_map<int,int> map;
         for(int i = 0; i < size; i++){
-            int k = target - nums[i];
-            for(int j = i+1 ; j<size; j++){
-                if(nums[j] == k){
-                ans.push_back(i);
-                ans.push_back(j);
-                }   
-            }
             
+            int k = target - nums[i];
+            if(map.find(k) != map.end()){
+                ans.push_back(map[k]);
+                ans.push_back(i);
+                return ans;
+            }
+            else
+                map[nums[i]] = i;   
         }
+        
         return ans;
+        
         
     }
 };
